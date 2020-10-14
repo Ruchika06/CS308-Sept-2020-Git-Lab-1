9abde049f4c009a4a98f9923608f068684bd1b16
ruchika06 <b18135@students.iitmandi.ac.in>
2020-10-14 10:47:48 +0530

    Input validated

--- sqrt.c
+++ sqrt.c
@@ -18,6 +18,11 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+	
+	if(input<0){
+		printf("Input not valid");
+		return (0);
+	}
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 
 	printf("End of program. Exiting.");
